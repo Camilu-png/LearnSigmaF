c1a7ea13c9ccbb10fd7c7c0a9a873f5459300dbf
Camila Arancibia <camilachillan99@gmail.com>
2021-08-28 19:57:38 -0400

    Changed the style from folder menu to bar menu

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,12 +1,8 @@
 function hideShow(){
     if(document.querySelector('.nav-left').classList.contains('is-active')){
         document.querySelector('.nav-left').classList.remove('is-active');
-        document.querySelector('.fa-folder').classList.add('is-active');
-        document.querySelector('.fa-folder-open').classList.remove('is-active');
     }
     else{
         document.querySelector('.nav-left').classList.add('is-active');
-        document.querySelector('.fa-folder').classList.remove('is-active');
-        document.querySelector('.fa-folder-open').classList.add('is-active');
     }   
 }
