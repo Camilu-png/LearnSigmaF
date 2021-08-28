1b064cbb4e71da1b584be816d40f31e643c9b7e1
Camila Arancibia <camilachillan99@gmail.com>
2021-08-28 19:39:44 -0400

    A menu was created for the mobile version

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -1,11 +1,11 @@
 function hideShow(){
-    if(document.querySelector('.menu').classList.contains('is-active')){
-        document.querySelector('.menu').classList.remove('is-active');
+    if(document.querySelector('.nav-left').classList.contains('is-active')){
+        document.querySelector('.nav-left').classList.remove('is-active');
         document.querySelector('.fa-folder').classList.add('is-active');
         document.querySelector('.fa-folder-open').classList.remove('is-active');
     }
     else{
-        document.querySelector('.menu').classList.add('is-active');
+        document.querySelector('.nav-left').classList.add('is-active');
         document.querySelector('.fa-folder').classList.remove('is-active');
         document.querySelector('.fa-folder-open').classList.add('is-active');
     }   
